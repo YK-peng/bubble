1c9f73687620fa7d912cc1f2f2e45eee8d1ec680
pengyangkang <pengyangkang@lukou.com>
2020-01-30 12:14:13 +0800

    auth server first commit

---
+++ auth/conf/conf.go
@@ -0,0 +1,80 @@
+package conf
+
+import (
+	"flag"
+	"os"
+	"strconv"
+	"time"
+
+	xtime "auth/xframe/time"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	confPath string
+	debug    bool
+
+	// Conf config
+	Conf *Config
+)
+
+func init() {
+	var (
+		defDebug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	)
+	flag.StringVar(&confPath, "conf", "conf/router-example.toml", "default config path.")
+	flag.BoolVar(&debug, "debug", defDebug, "server debug. or use DEBUG env variable, value: true/false etc.")
+}
+
+// Init init config.
+func Init() (err error) {
+	Conf = Default()
+	md, err := toml.DecodeFile(confPath, &Conf)
+	println(md.Keys())
+	return
+}
+
+// Default new a config with specified defualt value.
+func Default() *Config {
+	return &Config{
+		Debug: debug,
+		RPCServer: &RPCServer{
+			Network:           "tcp",
+			Addr:              ":3109",
+			Timeout:           xtime.Duration(time.Second),
+			IdleTimeout:       xtime.Duration(time.Second * 60),
+			MaxLifeTime:       xtime.Duration(time.Hour * 2),
+			ForceCloseWait:    xtime.Duration(time.Second * 20),
+			KeepAliveInterval: xtime.Duration(time.Second * 60),
+			KeepAliveTimeout:  xtime.Duration(time.Second * 20),
+		},
+		Xlog: &Xlog{
+			Path:  "default.log",
+			Level: "debug",
+		},
+	}
+}
+
+type Config struct {
+	Debug     bool
+	RPCServer *RPCServer
+	Xlog      *Xlog
+}
+
+// RPCServer is RPC server config.
+type RPCServer struct {
+	Network           string
+	Addr              string
+	Timeout           xtime.Duration
+	IdleTimeout       xtime.Duration
+	MaxLifeTime       xtime.Duration
+	ForceCloseWait    xtime.Duration
+	KeepAliveInterval xtime.Duration
+	KeepAliveTimeout  xtime.Duration
+}
+
+type Xlog struct {
+	Path  string
+	Level string
+}
---
+++ auth/grpc/server.go
@@ -0,0 +1,45 @@
+package grpc
+
+import (
+	"auth"
+	"auth/conf"
+	"context"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+// New auth grpc server.
+func New(c *conf.RPCServer, s *auth.Server) *grpc.Server {
+	keepParams := grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle:     time.Duration(c.IdleTimeout),
+		MaxConnectionAgeGrace: time.Duration(c.ForceCloseWait),
+		Time:                  time.Duration(c.KeepAliveInterval),
+		Timeout:               time.Duration(c.KeepAliveTimeout),
+		MaxConnectionAge:      time.Duration(c.MaxLifeTime),
+	})
+	srv := grpc.NewServer(keepParams)
+	RegisterAuthServer(srv, &server{s})
+	lis, err := net.Listen(c.Network, c.Addr)
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
+	return srv
+}
+
+type server struct {
+	srv *auth.Server
+}
+
+var _ AuthServer = &server{}
+
+func (s server) AuthRequest(context.Context, *ClientInfo) (*AuthRet, error) {
+	panic("implement me")
+}
